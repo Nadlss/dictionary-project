ade75bb08b5817192205231232b2beae128d2a6a
Nadlss <58627148+Nadlss@users.noreply.github.com>
2021-07-06 08:54:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './099-book-3.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo"/>
+      </header>
+    </div>
+  );
+}
+
+
