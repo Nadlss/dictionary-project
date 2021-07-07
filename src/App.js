6834aaa19da7dd2148697404e829479fa9eb5a0f
Nadlss <58627148+Nadlss@users.noreply.github.com>
2021-07-07 08:47:39 +0100

    Created initial structures
    - Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './099-book-3.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Susana Simões</small>
+      </footer>
+      </div>
     </div>
   );
 }
